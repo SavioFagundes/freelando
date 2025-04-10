5e8d34a78ddee774f485235c56cee711b53de670
SavioFagundes <akirasfm@gmail.com>
2025-04-10 12:22:58 -0300

    feat: Configuração inicial do Entity Framework e Swagger

--- Solution1/src/Freelando.Modelos/Especialidade.cs
+++ Solution1/src/Freelando.Modelos/Especialidade.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Freelando.Modelo;
+[Table("TB_Especialidades")]
 public class Especialidade
 {
+    [Column("Id_Especialidade")]
     public Guid Id { get; set; }
+    [Column("DS_Especialidade")]
     public string? Descricao { get; set; }
 
 }
